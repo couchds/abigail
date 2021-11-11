3eca774ec1e6328446f128916cdea0716a8bca5a
Daniel Couch <danielcouch1864@gmail.com>
2021-11-11 00:44:23 -0500

    add placeholder setup DAGs

---
+++ db/dags/setup_neo4j.py
@@ -0,0 +1,25 @@
+import datetime as dt
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+def setup_efetch():
+    print('Indexing nodes...')
+
+default_args = {
+    'owner': 'me',
+    'start_date': dt.datetime(2021, 11, 10),
+    'retries': 1,
+    'retry_delay': dt.timedelta(minutes=5),
+}
+
+with DAG('setup_neo4j',
+         default_args=default_args,
+         schedule_interval='0 * * * *',
+         ) as dag:
+
+    sleep = BashOperator(task_id='sleep',
+                         bash_command='sleep 5')
+    print_world = PythonOperator(task_id='setup_neo4j',
+                                 python_callable=setup_neo4j)
