f83c5db1ab06e52e26976f1dc0435101362fe313
Daniel Couch <danielcouch1864@gmail.com>
2021-11-16 00:49:41 -0500

    add setup_indices.py script

---
+++ db/dags/scripts/setup_indices.py
@@ -0,0 +1,35 @@
+from neo4j import GraphDatabase
+
+
+URI = 'neo4j://neo4j:7687' # use the neo4j container
+
+INDICES = [
+    {
+        'node_label': 'GO',
+        'property': 'uid'
+    },
+    {
+        'node_label': 'Gene',
+        'property': 'uid'
+    },
+    {
+        'node_label': 'Disease',
+        'property': 'uid'
+    }
+]
+
+
+def create_index_query(node_type, indexed_property):
+    """ Create the query for the index statement. See:
+    https://neo4j.com/docs/cypher-manual/current/indexes-for-search-performance/
+    """
+    print('here!!')
+    print(f'CREATE INDEX ON :{node_type}({indexed_property});')
+    return f'CREATE INDEX ON :{node_type}({indexed_property});'
+
+
+def setup_indices():
+    driver = GraphDatabase.driver(URI, auth=("neo4j", "tmppassword"), encrypted=False) # encrypted set to false for localhost
+    with driver.session() as session:
+        for index in INDICES:
+            session.run(create_index_query(index['node_label'], index['property']))
---
+++ db/dags/setup_database.py
@@ -0,0 +1,51 @@
+import datetime as dt
+
+from scripts import setup_indices
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+def setup_go():
+    print('Setting up GO nodes in database...')
+
+def setup_gene():
+    print('Setting up gene nodes in database...')
+
+def setup_disease():
+    print('Setting up disease nodes in database...')
+
+def setup_efetch():
+    print('Performing eFetch search...')
+
+
+default_args = {
+    'owner': 'me',
+    'start_date': dt.datetime(2021, 11, 10),
+    'retries': 1,
+    'retry_delay': dt.timedelta(minutes=5),
+}
+
+with DAG('setup_database',
+         default_args=default_args,
+         schedule_interval='0 * * * *',
+         ) as dag:
+
+    setup_indices_operator = PythonOperator(task_id='setup_indices_task',
+                                 python_callable=setup_indices.setup_indices)
+    sleep = BashOperator(task_id='sleep1',
+                         bash_command='sleep 5')
+    setup_go_operator = PythonOperator(task_id='setup_go',
+                                 python_callable=setup_go)
+    sleep = BashOperator(task_id='sleep2',
+                         bash_command='sleep 5')
+    setup_gene_operator = PythonOperator(task_id='setup_gene',
+                                 python_callable=setup_gene)
+    sleep = BashOperator(task_id='sleep3',
+                         bash_command='sleep 5')
+    setup_disease_operator = PythonOperator(task_id='setup_disease',
+                                 python_callable=setup_disease)
+    sleep = BashOperator(task_id='sleep4',
+                         bash_command='sleep 5')
+    setup_efetch_operator = PythonOperator(task_id='setup_efetch',
+                                 python_callable=setup_efetch)
