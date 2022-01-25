84a2270e3a77e5a6c8a1b4c5ecdae218da056c04
Daniel Couch <danielcouch1864@gmail.com>
2022-01-25 01:28:25 -0500

    get setup scripts all working

---
+++ db/dockerfiles/neo4j/setup_gos.py
@@ -0,0 +1,27 @@
+from neo4j import GraphDatabase
+
+URI = 'neo4j://neo4j:7687'
+
+def setup_gos():
+    query = '''
+        USING PERIODIC COMMIT 500
+        LOAD CSV WITH HEADERS FROM 'file:///go-basic.tsv' AS row FIELDTERMINATOR '\t' 
+        WITH row  WHERE row.name IS NOT NULL AND row.synonyms IS NOT NULL
+        MERGE (g:GO {uid: row.id, name: row.name, namespace: row.namespace, definition: row.definition})
+        WITH g, row
+        UNWIND split(row.synonyms, ",") as synonym
+        MERGE (s: Synonym {name: synonym})
+    '''
+   # WITH g, SPLIT(row.synonyms, ",") AS go_synonyms
+    # WITH g, SPLIT(row[4], ",") AS go_synonyms
+    #    UNWIND go_synonyms as go_synonym
+    #    MERGE (s: Synonym {name: go_synonym})
+    #    MERGE (g)-[:has_synonym]->(s)
+    #
+    #with open('')
+    print('here!')
+    driver = GraphDatabase.driver(URI, auth=("neo4j", "tmppassword"), encrypted=False) # encrypted set to false for localhost
+    with driver.session() as session:
+        session.run(query)
+
+setup_gos()
