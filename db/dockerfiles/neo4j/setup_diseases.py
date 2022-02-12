49d355adf21db51622a3732dd29c5bac47f6cab9
Daniel Couch <danielcouch1864@gmail.com>
2022-02-11 22:58:44 -0500

    literature mining working (#27)

--- db/dockerfiles/neo4j/setup_diseases.py
+++ db/dockerfiles/neo4j/setup_diseases.py
@@ -7,6 +7,9 @@ def setup_diseases():
         USING PERIODIC COMMIT 
         LOAD CSV WITH HEADERS FROM 'file:///doid.tsv' AS row FIELDTERMINATOR '\t' with row where row.definition is not null
         MERGE (d:Disease {uid: row.id, name: row.name, definition: row.definition})
+        WITH d, row
+        MERGE (s1: Synonym {name: row.name})
+        MERGE (d)-[:has_synonym]->(s1)
         WITH d, SPLIT(row.synonyms, "&&") AS disease_synonyms
         UNWIND disease_synonyms as disease_synonym
         MERGE (s: Synonym {name: disease_synonym})
--- db/dockerfiles/neo4j/setup_genes.py
+++ db/dockerfiles/neo4j/setup_genes.py
@@ -7,10 +7,13 @@ def setup_genes():
         USING PERIODIC COMMIT 
         LOAD CSV WITH HEADERS FROM 'file:///gene_output.tsv' AS row FIELDTERMINATOR '\t'
         MERGE (g:Gene {uid: row.id, symbol: row.approved_symbol, name: row.approved_name})
+        WITH g, row
+        MERGE (s1: Synonym {name: row.approved_symbol})
+        MERGE (g)-[:has_synonym]->(s1)
         WITH g, SPLIT(row.synonyms, "&") AS gene_synonyms
         UNWIND gene_synonyms AS synonym
-        MERGE (s: Synonym {name: synonym})
-        MERGE (g)-[:has_synonym]->(s)
+        MERGE (s2: Synonym {name: synonym})
+        MERGE (g)-[:has_synonym]->(s2)
     '''
     driver = GraphDatabase.driver(URI, auth=("neo4j", "tmppassword"), encrypted=False) # encrypted set to false for localhost
     with driver.session() as session:
