5d1a89b3a6f1fd3ebc001284d19d872b520061bb
Daniel Couch <danielcouch1864@gmail.com>
2022-02-14 19:31:24 -0500

    update app

--- web/abigail/pages/_app.js
+++ web/abigail/pages/_app.js
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
+//import '../styles/globals.css'
 import 'antd/dist/antd.css';
 
+import { Layout, PageHeader } from 'antd';
+const { Header, Content, Footer } = Layout;
+
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Header style={{ textAlign: 'center' }}>
+        <div>
+        <Title type="secondary" style={{ color: 'white' }}>ABIGAIL</Title>
+        </div>
+      </Header>
+      <Component {...pageProps} />
+    </Layout>
+  )
+  
 }
 
 export default MyApp
